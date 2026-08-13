46b8e5c9cc6534e421e4f5d2da68577e1cdc6d66
Snizhana <snizhanapetrushka@gmail.com>
2026-08-13 11:21:35 +0200

    Add ToastContainer to enable toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from 'react-toastify';
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Teachers from './pages/Teachers/Teachers';
@@ -14,6 +15,7 @@ export default function App() {
             <Route path="/teachers" element={<Teachers />} />
           </Route>
         </Routes>
+           <ToastContainer position="top-center" autoClose={3000} />
       </AuthProvider>
     </BrowserRouter>
   );
